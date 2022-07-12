b04f127b0908665595da5c84e3777d5001836bf5
imndx <imndxx@gmail.com>
2022-07-12 20:31:47 +0800

    支持发送消息

--- main.js
+++ main.js
@@ -25,12 +25,27 @@ function createWindow () {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   console.log('app ready!')
-  let userId = 'SkppUpPP';
-  let token = 'OOVf3IG0+Lwuvp/APioc98YszNAZfrU8RNyVSVJoOI51eOp40aeR13OtjSdCYohhJPb5nVQcw5RAdwWxw1AAXOjBLxtt/FDuvGLOZKPgnge/KAKFvKseeNzUQn/RB/6ieUCeq01aB5cfJInWiby8gZxofW8CpB9FDsawqBgFKFI=';
+  let userId = 'uiuJuJcc';
+  let token = 'czRvxoCnOAdtGxgSnl7C75zZS42wtb7BMNy+2/R/sNXBZBXX3c+GcalcgNTqhhquG0tOgoC5/O9UNHSjJSIGfJdCRN7y1FX4YMtFtZZ0hN8Gf070I+AETN4ZSgKUMvZ+VFlWmEyNJsVgaNxgeqe92a6OPl3ZjEgPKrkbxMtmA9Y='
   //cid = 182DB2AC-6792-4A2F-BDDC-04A57826F3A4
   let cid = proto.getClientId()
   console.log('client id is ', cid)
+  proto.setConnectionStatusListener((status) => {
+    console.log('---connectStatus changed', status);
+
+    if(status === 1){
+        let count = 0;
+        setInterval(() => {
+            count ++;
+            sendConversationMessage(count);
+        }, 200)
+
+    }
+  });
+
   proto.connect(userId, token);
+
+  console.log('connect---------');
 
   createWindow()
 
@@ -47,6 +62,24 @@ app.whenReady().then(() => {
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
+
+function sendConversationMessage(count) {
+    let strConv = '{"type":0,"conversationType":0,"target":"GNMtGtZZ","line":0}'
+    let strCont = `{"type":1,"searchableContent":"${count}","mentionedType":0,"mentionedTargets":[]}`;
+    proto.sendMessage(strConv, strCont, [], 0, (messageId, timestamp) => {
+        console.log('onPrepared', messageId, timestamp);
+    },
+    (uploaded, total) => {
+        console.log('onProgress', uploaded, total);
+    },
+    (messageUid, timestamp) => {
+        console.log('send success', messageUid, timestamp, count);
+    }, (err) => {
+        console.log('send fail', err);
+    }
+     );
+}
+
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
